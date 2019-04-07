555940944a39d1b15a3d12ba98ae497bae65a83c
snowaver <1123009419@qq.com>
2019-04-07 21:48:40 +0800

    1/remove attach base context in abstract activity.

--- app/src/main/java/cc/mashroom/squirrel/parent/AbstractActivity.java
+++ app/src/main/java/cc/mashroom/squirrel/parent/AbstractActivity.java
@@ -1,13 +1,8 @@
 package cc.mashroom.squirrel.parent;
 
 import  android.app.Activity;
-import  android.content.Context;
 import  android.content.Intent;
-import  android.content.res.Configuration;
-import  android.os.Build;
 import  android.widget.Toast;
-
-import  java.util.Locale;
 
 import  cc.mashroom.util.ObjectUtils;
 import  cc.mashroom.hedgehog.util.ContextUtils;
@@ -15,15 +10,6 @@ import  es.dmoral.toasty.Toasty;
 
 public  abstract  class  AbstractActivity  extends  cc.mashroom.hedgehog.parent.AbstractActivity
 {
-	protected  void  attachBaseContext(  Context  newBase )
-	{
-		Configuration  configuration = newBase.getResources().getConfiguration();
-
-		configuration.setLocale( Locale.forLanguageTag(newBase.getSharedPreferences("CONFIGURATION",MODE_PRIVATE).getString("LOCAL",Locale.ENGLISH.toLanguageTag())) );
-
-		super.attachBaseContext( Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ? super.createConfigurationContext(configuration) : newBase );
-	}
-
 	public  void  putResultDataAndFinish( Activity  context,int  resultCode,Intent  resultData )
 	{
 		context.setResult( resultCode,resultData );
